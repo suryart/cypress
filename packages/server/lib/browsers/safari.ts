d5440768126dc3d2eb7882ce89dcee29225fd771
Surya Tripathi <raj.surya19@gmail.com>
2020-03-16 20:39:47 +0530

    adding first commit for safari.ts file to support Safari browser

---
+++ packages/server/lib/browsers/safari.ts
@@ -0,0 +1,8 @@
+import Debug from 'debug'
+import { Browser } from './types'
+
+const debug = Debug('cypress:server:browsers:safari')
+
+export async function open (browser: Browser, url, options: any = {}) {
+  debug('TODO: add Safari launch code here')
+}
